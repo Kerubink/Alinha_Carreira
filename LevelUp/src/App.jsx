94d1c650f43c1b8d9df4eb500ac87e72364f27f7
Kauã Kelvyn <ckaua504@gmail.com>
2024-09-20 17:28:20 -0300

    adicionando aquivos e navbar

---
+++ LevelUp/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { Header } from './components/Header/header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>ola mundo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ LevelUp/src/components/Header/header.jsx
@@ -0,0 +1,84 @@
+import styles from "./header.module.css";
+import logo from "../../assets/icon.png"
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img
+          src={logo}
+          alt="logo"
+        />
+        <h1>
+          Level<span>Up</span>
+        </h1>
+      </div>
+
+      <nav>
+        <ul className={styles.links}>
+          <li>
+            <a href="/">
+              <button className={styles.button}>
+                <div className={styles.icone}>
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/3356/3356068.png"
+                    alt="icone"
+                  />
+                </div>
+                <span className={styles.lable}>Sobre</span>
+              </button>
+            </a>
+          </li>
+          <li>
+            <a href="/about">
+              <button className={styles.button}>
+                <div className={styles.icone}>
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/1197/1197495.png"
+                    alt="icone"
+                  />
+                </div>
+                <span className={styles.lable}>Desafios</span>
+              </button>
+            </a>
+          </li>
+          <li>
+            <a href="/contact">
+              <button className={styles.button}>
+                <div className={styles.icone}>
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/14820/14820458.png"
+                    alt="icone"
+                  />
+                </div>
+                <span className={styles.lable}>Missão</span>
+              </button>
+            </a>
+          </li>
+          <li>
+            <a href="/blog">
+              <button className={styles.button}>
+                <div className={styles.icone}>
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/1628/1628629.png"
+                    alt="icone"
+                  />
+                </div>
+                <span className={styles.lable}>Comentarios</span>
+              </button>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <a href="/github">
+        <button className={styles.gitButton}>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/3291/3291695.png"
+            alt="GitHub"
+          />
+          <span>Ver no Github</span>
+        </button>
+      </a>
+    </header>
+  );
+};
