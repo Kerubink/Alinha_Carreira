2ce95e3efee42b00435a793d2f28e79013efefd9
Kauã Kelvyn <ckaua504@gmail.com>
2024-09-22 11:17:00 -0300

    fazendo requisições na api de gameficação pra buscar os desafios disponiveis

--- LevelUp/src/components/Sections/DailyChallenges/dailyChallenges.jsx
+++ LevelUp/src/components/Sections/DailyChallenges/dailyChallenges.jsx
@@ -1,12 +1,44 @@
+import React, { useEffect, useState } from "react";
 import styles from "./dailyChallenges.module.css";
 
 export const DailyChallenges = () => {
-    return (
-        <section id="dailyChallenges" className={styles.dailyChallenges}>
-            <h2>Daily Challenges</h2>
-            <div className={styles.challengesContainer}>
-                {/* Challenge cards */}
-            </div>
-        </section>
-    )
-}
+  const [challenges, setChallenges] = useState([]);
+
+  useEffect(() => {
+    const fetchChallenges = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/challenges");
+        const data = await response.json(); 
+        setChallenges(data); 
+      } catch (error) {
+        console.error("Erro ao buscar desafios:", error);
+      }
+    };
+
+    fetchChallenges(); 
+  }, []); 
+  return (
+    <section id="dailyChallenges" className={styles.dailyChallenges}>
+      <h2>Daily Challenges</h2>
+      <div className={styles.challengesContainer}>
+        <div className={styles.challengesContainerHeader}>
+          <h3>Lista de tasks</h3>
+        </div>
+        <div className={styles.challengesContainerBody}>
+          <div className={styles.challenges}>
+            {challenges.length > 0 ? (
+              challenges.map((challenge) => (
+                <div key={challenge.id} className={styles.challenge}>
+                  <h4>{challenge.title}</h4>
+                </div>
+              ))
+            ) : (
+              <p>Carregando desafios...</p> 
+            )}
+          </div>
+        </div>
+      </div>
+      <div className={styles.challengesInfo}></div>
+    </section>
+  );
+};
